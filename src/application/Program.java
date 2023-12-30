e66913b4ec30033447c3c0544911591e6a587e25
Raphael Gomes <rafagomes1706@gmail.com>
2023-12-30 11:26:02 -0300

    tratamento das exceções e limpando a tela quando altera o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.BoardException;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,16 +16,25 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("source: ");
-            ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
 
-            ChessPiece captueredPiece = chessMatch.performChessMove(source, target);
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece captueredPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+
         }
 
 
