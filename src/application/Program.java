f11f536ac6833043456cf9c67dbbe7fe2199493b
Raphael Gomes <rafagomes1706@gmail.com>
2024-01-06 11:19:20 -0300

    mostra os movimentos possiveis da peça selecionada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import boardgame.BoardException;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -24,6 +23,10 @@ public class Program {
                 System.out.println();
                 System.out.print("source: ");
                 ChessPosition source = UI.readChessPosition(sc);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
                 System.out.println();
                 System.out.print("Target: ");
