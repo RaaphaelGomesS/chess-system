b9c9e3b1cbb09378d481a8ff2d1b647f561ca376
Raphael Gomes <rafagomes1706@gmail.com>
2024-01-06 15:24:39 -0300

    mostrando as peças capturadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,12 +5,15 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captureds = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
@@ -19,7 +22,7 @@ public class Program {
             try {
                 UI.clearScreen();
 
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captureds);
                 System.out.println();
                 System.out.print("source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -32,7 +35,12 @@ public class Program {
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
-                ChessPiece captueredPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captureds.add(capturedPiece);
+                }
+
             } catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
